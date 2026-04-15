6a40483c9a8d072dc42e99d68e94e7ab42373e3f
pkm021118 <pkm021117@gmail.com>
2026-04-15 16:21:29 +0900

    refactor: 프로젝트 상세 헤더 구성 정리

--- frontend/src/app/(project)/projects/[projectId]/_component/ProjectDetailHeader.tsx
+++ frontend/src/app/(project)/projects/[projectId]/_component/ProjectDetailHeader.tsx
@@ -1,21 +1,21 @@
-import { OnlineUsers, type OnlineUserTypes } from './OnlineUsers';
+import { Users, type UserInfo } from '../../../../../components/commons/UserAvatarGroup';
 import { ProjectViewTabs, type ProjectViewTypes } from './ProjectViewTabs';
 
 interface ProjectDetailHeaderProps {
-  activeView: ProjectViewTypes;
-  onlineUsers: OnlineUserTypes[];
-  onViewChange: (view: ProjectViewTypes) => void;
+    activeView: ProjectViewTypes;
+    onlineUsers: readonly UserInfo[];
+    onViewChange: (view: ProjectViewTypes) => void;
 }
 
 export const ProjectDetailHeader = ({
-  activeView,
-  onlineUsers,
-  onViewChange,
-}: ProjectDetailHeaderProps) => {
-  return (
-    <header className="relative z-10 flex h-14 shrink-0 items-center justify-between overflow-visible border-b border-line-soft bg-static-white px-4 pt-4.5 pb-4">
-      <ProjectViewTabs activeView={activeView} onViewChange={onViewChange} />
-      <OnlineUsers users={onlineUsers} />
-    </header>
-  );
+                                        activeView,
+                                        onlineUsers,
+                                        onViewChange,
+                                    }: ProjectDetailHeaderProps) => {
+    return (
+        <header className="relative z-10 flex h-14 shrink-0 items-center justify-between overflow-visible border-b border-line-soft bg-static-white px-4 pt-4.5 pb-4">
+            <ProjectViewTabs activeView={activeView} onViewChange={onViewChange} />
+            <Users users={onlineUsers} mode={"active"}/>
+        </header>
+    );
 };
