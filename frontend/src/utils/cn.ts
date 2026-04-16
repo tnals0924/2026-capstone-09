a636c3c1a189e462c20ab60ac60ab7f8e8055cfa
pkm021118 <pkm021117@gmail.com>
2026-04-16 11:34:48 +0900

    fix: cn 유틸 기존으로 복구

--- frontend/src/utils/cn.ts
+++ frontend/src/utils/cn.ts
@@ -1,34 +1,5 @@
 import { clsx, type ClassValue } from 'clsx';
-import { extendTailwindMerge } from 'tailwind-merge';
-
-const customTwMerge = extendTailwindMerge({
-  extend: {
-    classGroups: {
-      'font-size': [
-        'text-display-1',
-        'text-display-2',
-        'text-display-3',
-        'text-title-1',
-        'text-title-2',
-        'text-title-3',
-        'text-heading-1',
-        'text-heading-2',
-        'text-headline-1',
-        'text-headline-2',
-        'text-body-1',
-        'text-body-1-reading',
-        'text-body-2',
-        'text-body-2-reading',
-        'text-label-1',
-        'text-label-1-reading',
-        'text-label-2',
-        'text-caption-1',
-        'text-caption-2',
-      ],
-    },
-  },
-});
 
 export function cn(...inputs: ClassValue[]) {
-  return customTwMerge(clsx(inputs));
+  return clsx(inputs);
 }
