dd462d048f3e102ebcc5533ab7ee069869b31f70
sinji2102 <sinzi031012@naver.com>
2026-04-17 17:00:40 +0900

    feat: 커스텀 메뉴 아이템 구현

--- frontend/src/app/design-system-test/page.tsx
+++ frontend/src/app/design-system-test/page.tsx
@@ -1,41 +1,31 @@
 'use client';
 
-import {
-  Alert,
-  AlertActionArea,
-  AlertActionAreaButton,
-  AlertContainer,
-  AlertContent,
-  AlertDescription,
-  AlertHeading,
-  Button,
-} from '@wanteddev/wds';
-import { useState } from 'react';
+import { Button, Menu, MenuContent, MenuList, MenuTrigger } from '@wanteddev/wds';
+import { IconAgent, IconAiReview, IconPlus } from '@wanteddev/wds-icon';
+import { CustomMenu } from '@/components/commons/custom-menu/CustomMemu';
 
 const Demo = () => {
-  const [open, setOpen] = useState(false);
-  const [result, setResult] = useState<string | null>(null);
-
   return (
     <>
-      <Alert open={open} onOpenChange={setOpen}>
-        <AlertContainer onDismiss={() => setResult('Dismissed')}>
-          <AlertContent>
-            <AlertHeading>Heading</AlertHeading>
-            <AlertDescription>Description</AlertDescription>
-          </AlertContent>
-          <AlertActionArea>
-            <AlertActionAreaButton variant="assistive" onClick={() => setResult('Cancel')}>
-              Cancel
-            </AlertActionAreaButton>
-            <AlertActionAreaButton variant="normal" onClick={() => setResult('Confirm')}>
-              Confirm
-            </AlertActionAreaButton>
-          </AlertActionArea>
-        </AlertContainer>
-      </Alert>
+      <Menu>
+        <MenuTrigger>
+          <Button>Click me</Button>
+        </MenuTrigger>
 
-      <Button onClick={() => setOpen(true)}>Alert 테스트</Button>
+        <MenuContent sx={{ width: '154px' }}>
+          <MenuList>
+            <CustomMenu value="item1" icon={<IconPlus />} onClick={() => console.log('test')}>
+              Item 1
+            </CustomMenu>
+            <CustomMenu value="item2" icon={<IconAiReview />}>
+              Item 2
+            </CustomMenu>
+            <CustomMenu value="item3" icon={<IconAgent />}>
+              Item 3
+            </CustomMenu>
+          </MenuList>
+        </MenuContent>
+      </Menu>
     </>
   );
 };
