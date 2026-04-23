4bd0e9129f9e82d42f2500aeddf78a5470c03096
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-04-23 21:45:36 +0900

    refactor: 담당자 추가 응답 HTTP status 200으로 통일 - #76

--- backend/flowmeet-api/src/main/java/kr/flowmeet/api/node/controller/NodeAssigneeController.java
+++ backend/flowmeet-api/src/main/java/kr/flowmeet/api/node/controller/NodeAssigneeController.java
@@ -2,13 +2,11 @@ package kr.flowmeet.api.node.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import kr.flowmeet.api.common.dto.CommonResponse;
 import kr.flowmeet.api.node.dto.request.CreateAssigneeRequest;
@@ -25,7 +23,6 @@ public class NodeAssigneeController implements NodeAssigneeApi {
 
     @Override
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public CommonResponse<?> createAssignee(
             @UserId Long userId,
             @PathVariable Long projectId,
