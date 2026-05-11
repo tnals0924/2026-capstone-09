d3382e009c37e94195871102460f5b0a105b3a38
wngktjd13 <seonguk3553@kookmin.ac.kr>
2026-05-11 21:16:04 +0900

    chore: MCP Client 연결 URL 설정

--- ai/agent/main.py
+++ ai/agent/main.py
@@ -8,7 +8,7 @@ async def main():
     mcp_client = MCPClient()
  
     try:
-        await mcp_client.connect_sse("")  #mcp server url 추가해주세요
+        await mcp_client.connect_sse("http://localhost:8082/sse")
  
         agent = Agent(mcp_client=mcp_client)
  
